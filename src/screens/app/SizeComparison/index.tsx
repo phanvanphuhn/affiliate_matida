d87942176a6c4184832a7767deb95120c9da6f35
mison201 <mison201@gmail.com>
2023-07-19 09:47:36 +0700

    chore: add quizz, posts session to baby tracker detail

--- src/screens/app/SizeComparison/index.tsx
+++ src/screens/app/SizeComparison/index.tsx
@@ -1,24 +1,31 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {View, Text, ScrollView, FlatList, ListRenderItem} from 'react-native';
-import {styles} from './styles';
-import {Header, PickerWeek, ViewButton} from '@component';
-import {SvgArrowLeft} from '@images';
-import {useTranslation} from 'react-i18next';
-import {colors} from '@stylesCommon';
-import {useSelector} from 'react-redux';
-import {GlobalService, getSizeComparison} from '@services';
+import React, { useEffect, useRef, useState } from 'react';
+import { View, Text, ScrollView, FlatList, ListRenderItem, TouchableOpacity } from 'react-native';
+import { styles } from './styles';
+import { Header, PickerWeek, ViewButton } from '@component';
+import { SvgArrowLeft, SvgMessages3 } from '@images';
+import { useTranslation } from 'react-i18next';
+import { colors } from '@stylesCommon';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  updateDataHome,
+} from '@redux';
+import { GlobalService, getSizeComparison, answerDailyQuiz } from '@services';
 
-import {Body} from './component/Body';
-import {Embryo} from './component/Embryo';
-import {Size} from './component/Size';
-import {useRoute} from '@react-navigation/native';
-import {trackingAppEvent, event, useUXCam} from '@util';
-import {ROUTE_NAME} from '@routeName';
+import { Body } from './component/Body';
+import { Embryo } from './component/Embryo';
+import { Size } from './component/Size';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { trackingAppEvent, event, useUXCam } from '@util';
+import { ROUTE_NAME } from '@routeName';
+import { ListPostComponent } from '../Home/ListPostComponent';
+import { ViewQuiz, BannerTestQuiz } from '../Home/components';
+
 
 const SizeComparison = () => {
+  const dispatch = useDispatch();
   const route = useRoute<any>();
-  const {option} = route?.params;
-  const {t} = useTranslation();
+  const { option } = route?.params;
+  const { t } = useTranslation();
   const [status, setStatus] = useState(option);
   const [data, setData] = useState<any>(null);
   const [weekLocal, setWeekLocal] = useState(null);
@@ -26,6 +33,10 @@ const SizeComparison = () => {
   useUXCam(ROUTE_NAME.SIZE_COMPARISON);
 
   const refList = useRef<FlatList>(null);
+
+  const loading = useSelector((state: any) => state?.home?.loading);
+  const dataHome = useSelector((state: any) => state?.home?.data);
+  const navigation = useNavigation<any>();
 
   const week =
     useSelector(
@@ -37,7 +48,22 @@ const SizeComparison = () => {
       case 1:
         return <Size data={data?.baby_size} week={weekLocal} />;
       case 2:
-        return <Embryo data={data?.baby} week={weekLocal} />;
+        return <View>
+          <Embryo data={data?.baby} week={weekLocal} />
+
+          <ListPostComponent loading={loading} posts={dataHome?.posts} />
+
+          <TouchableOpacity
+            style={styles.createPostButton}
+            onPress={() => navigation.navigate(ROUTE_NAME.CREATE_NEWPOST)}>
+            <SvgMessages3 />
+            <Text style={styles.titleButton}>{t('home.createPost')}</Text>
+          </TouchableOpacity>
+
+          {dataHome?.dailyQuizz ? <ViewQuiz onAnswer={onAnswerQuiz} /> : null}
+
+          <BannerTestQuiz />
+        </View>;
       case 3:
         return <Body data={data?.mom} week={weekLocal} />;
       default:
@@ -50,10 +76,29 @@ const SizeComparison = () => {
     try {
       GlobalService.showLoading();
       setTimeout(() => {
-        refList?.current?.scrollToOffset({animated: true, offset: 0});
+        refList?.current?.scrollToOffset({ animated: true, offset: 0 });
       }, 50);
       const res = await getSizeComparison(value);
       setData(res.data);
+      GlobalService.hideLoading();
+    } catch (error) {
+      GlobalService.hideLoading();
+    }
+  };
+
+  const onAnswerQuiz = async (value: any) => {
+    try {
+      GlobalService.showLoading();
+      const res = await answerDailyQuiz(value);
+      dispatch(
+        updateDataHome({
+          ...data,
+          dailyQuizz: {
+            ...data?.dailyQuizz,
+            ...res?.data,
+          },
+        }),
+      );
       GlobalService.hideLoading();
     } catch (error) {
       GlobalService.hideLoading();
@@ -94,8 +139,20 @@ const SizeComparison = () => {
         bounces={false}
         ListFooterComponent={renderView}
       />
+
+      {/* <View>
+        <ListPostComponent loading={loading} posts={dataHome?.posts} />
+
+        <TouchableOpacity
+          style={styles.createPostButton}
+          onPress={() => navigation.navigate(ROUTE_NAME.CREATE_NEWPOST)}>
+          <SvgMessages3 />
+          <Text style={styles.titleButton}>{t('home.createPost')}</Text>
+        </TouchableOpacity>
+      </View> */}
+
     </View>
   );
 };
 
-export {SizeComparison};
+export { SizeComparison };
--- src/screens/app/SizeComparison/styles.tsx
+++ src/screens/app/SizeComparison/styles.tsx
@@ -1,5 +1,5 @@
-import {StyleSheet} from 'react-native';
-import {scaler, colors, stylesCommon} from '@stylesCommon';
+import { StyleSheet } from 'react-native';
+import { scaler, colors, stylesCommon } from '@stylesCommon';
 
 const styles = StyleSheet.create({
   container: {
@@ -8,6 +8,24 @@ const styles = StyleSheet.create({
   containerPicker: {
     marginTop: scaler(16),
   },
+  createPostButton: {
+    marginHorizontal: scaler(20),
+    width: undefined,
+    backgroundColor: colors.white,
+    marginBottom: scaler(40),
+    flexDirection: 'row',
+    height: scaler(48),
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: scaler(8)
+  },
+  titleButton: {
+    fontSize: scaler(14),
+    ...stylesCommon.fontWeight600,
+    lineHeight: scaler(21),
+    color: colors.primary,
+    marginLeft: scaler(11)
+  }
 });
 
-export {styles};
+export { styles };
