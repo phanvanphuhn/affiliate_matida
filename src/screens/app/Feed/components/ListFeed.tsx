e115964b9c7b894343940919a06368f2dd47a4d1
phu phan <pro14inch@PVP.local>
2023-08-01 18:34:52 +0700

    done ui list feed screen

--- src/screens/app/Feed/components/ListFeed.tsx
+++ src/screens/app/Feed/components/ListFeed.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { View, Text, FlatList, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import { IDataListFeed } from '../type'
-import { colors, scaler, stylesCommon } from '@stylesCommon'
+import { colors } from '@stylesCommon'
+import {styles} from '../styles'
+import {iconClock, SvgEye} from '@images';
 
 const ListFeed = (props: any) => {
   const {data} = props
@@ -15,12 +17,16 @@ const ListFeed = (props: any) => {
             style={styles.image}
           /> 
           <View style={styles.leftDescription}>
-            <Text style={styles.description}>
+            <Image source={iconClock} />
+
+            <Text style={styles.description} numberOfLines={1}>
               {item.duration} mins watch
             </Text>
           </View>
           <View style={styles.rightDescription}>
-            <Text style={styles.description}>
+            <SvgEye stroke={colors.borderColor} />
+
+            <Text style={styles.description} numberOfLines={1}>
               {item.view} views
             </Text>
           </View>
@@ -28,9 +34,16 @@ const ListFeed = (props: any) => {
         <Text style={styles.title}>
           {item.title}
         </Text>
-        <Text style={styles.subTitle}>
-          Coach by <Text style={{color: colors.success_message}}>{item.author}</Text> 
-        </Text>
+        <View style={styles.wrapAvatarContainer}>
+          <Image
+            source={{uri: item.image}}
+            style={styles.imageAvatar}
+          /> 
+
+          <Text style={styles.subTitle}>
+            Coach by <Text style={{color: colors.success_message}}>{item.author}</Text> 
+         </Text>  
+        </View>        
       </TouchableOpacity>
     )
   }
@@ -47,49 +60,5 @@ const ListFeed = (props: any) => {
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container:{
-    backgroundColor: 'white',
-    paddingBottom: 250,
-    padding: 8
-  },
-  itemContainer:{
-    flex: 0.5,
-    padding: 8
-  },
-  image:{
-    height: 220,
-    borderTopLeftRadius: 8,
-    borderTopRightRadius: 8
-  },
-  leftDescription:{
-    position: 'absolute',
-    bottom: 16,
-    left: 8,
-  },
-  rightDescription:{
-    position: 'absolute',
-    right: 24,
-    bottom: 16
-  },
-  description:{
-    color: colors.gray,
-    fontSize: scaler(9),
-    ...stylesCommon.fontWeight400,
-  },
-  title:{
-    color: colors.black,
-    fontSize: scaler(12),
-    ...stylesCommon.fontWeight500,
-    marginTop: 8
-  },
-  subTitle:{
-    color: colors.textSmallColor,
-    fontSize: scaler(10),
-    ...stylesCommon.fontWeight400,
-    marginTop: 8
-  }
-})
 
 export default ListFeed;
