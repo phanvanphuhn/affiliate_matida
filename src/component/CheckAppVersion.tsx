0dd29579ff1da2c788dc10ded0755cb434c4f5b8
phu phan <pro14inch@PVP.local>
2023-09-01 10:41:45 +0700

    force update

--- src/component/CheckAppVersion.tsx
+++ src/component/CheckAppVersion.tsx
@@ -132,7 +132,7 @@ export const CheckAppVersion = (props: CheckAppVersionProps) => {
       hideModalContentWhileAnimating
       backdropTransitionOutTiming={0}
       backdropOpacity={0.7}
-      onBackdropPress={() => setIsVisible(false)}
+      // onBackdropPress={() => setIsVisible(false)}
       isVisible={isVisible}>
       <View style={styles.viewContent}>
         <Text
