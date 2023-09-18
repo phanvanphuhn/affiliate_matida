2643ed35bcb8187e26f5822fb96feb57c52275b5
phu phan <>
2023-09-18 11:26:59 +0700

    add expert touchopacity

--- src/screens/app/DetailFeed/components/TitleFeed.tsx
+++ src/screens/app/DetailFeed/components/TitleFeed.tsx
@@ -1,30 +1,37 @@
-import {colors, heightScreen, scaler} from '@stylesCommon';
+import {LogoApp} from '@images';
+import {useNavigation} from '@react-navigation/native';
+import {ROUTE_NAME} from '@routeName';
+import {colors, heightScreen, scaler, stylesCommon} from '@stylesCommon';
 import React, {useEffect, useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import {
+  Linking,
   ScrollView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   TouchableWithoutFeedback,
   View,
 } from 'react-native';
+import FastImage from 'react-native-fast-image';
 import RenderHtml from 'react-native-render-html';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {IDataListFeed} from '../../Feed/type';
 import {useVideo} from './Container';
 import CustomImageRenderer from './CustomImageRenderer';
 import clip from './clip';
-import { tagsStyles } from './settingsHtml';
+import {tagsStyles} from './settingsHtml';
+
 interface TitleFeedProps {
   item: IDataListFeed;
 }
 const TitleFeed = (props: TitleFeedProps) => {
   const {t} = useTranslation();
-
+  const navigation = useNavigation<any>();
   const [textShown, setTextShown] = useState(false);
   const [lengthMore, setLengthMore] = useState(false);
   const insets = useSafeAreaInsets();
-  const {state} = useVideo();
+  const {state, setState} = useVideo();
   const toggleNumberOfLines = () => {
     setTextShown(!textShown);
   };
@@ -58,6 +65,21 @@ const TitleFeed = (props: TitleFeedProps) => {
       html: true,
     });
   };
+
+  const renderersProps = {
+    a: {
+      onPress(event, url, htmlAttribs, target) {
+        Linking.openURL(url);
+      },
+    },
+  };
+
+  const onPressExpertContainer = (item: IDataListFeed) => {
+    navigation.navigate(ROUTE_NAME.DETAIL_USER, {
+      id: item?.expert_id,
+    });
+  };
+
   return (
     <View
       style={[
@@ -91,6 +113,24 @@ const TitleFeed = (props: TitleFeedProps) => {
         <Text style={[styles.title, textShown ? styles.colorWhite : {}]}>
           {props.item.title}
         </Text>
+        <TouchableOpacity
+          style={styles.wrapAvatarContainer}
+          onPress={() => onPressExpertContainer(props.item)}
+          disabled={props.item.expert_id ? false : true}>
+          <FastImage
+            source={
+              props.item.expert_image ? {uri: props.item.expert_image} : LogoApp
+            }
+            style={styles.imageAvatar}
+            resizeMode="contain"
+          />
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Text style={styles.subTitle}>{t('feed.by')}</Text>
+            <Text style={{color: colors.success_message, marginLeft: 8}}>
+              {props.item.expert_name ? props.item.expert_name : 'Matida'}
+            </Text>
+          </View>
+        </TouchableOpacity>
         {!!getFullDescription() && (
           <ScrollView
             style={{maxHeight: '100%'}}
@@ -102,6 +142,7 @@ const TitleFeed = (props: TitleFeedProps) => {
               renderers={{
                 img: CustomImageRenderer,
               }}
+              renderersProps={renderersProps}
               source={{
                 html: `<div>${
                   textShown ? getFullDescription() : getDescription(150)
@@ -165,5 +206,32 @@ const styles = StyleSheet.create({
     bottom: 0,
     maxHeight: heightScreen,
     justifyContent: 'flex-end',
+  },
+  wrapAvatarContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  imageAvatar: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    marginRight: 8,
+  },
+  subTitle: {
+    color: colors.textSmallColor,
+    fontSize: scaler(14),
+    ...stylesCommon.fontWeight400,
+  },
+  fadingContainer: {
+    padding: 20,
+    backgroundColor: 'powderblue',
+  },
+  fadingText: {
+    fontSize: 28,
+  },
+  buttonRow: {
+    flexBasis: 100,
+    justifyContent: 'space-evenly',
+    marginVertical: 16,
   },
 });
