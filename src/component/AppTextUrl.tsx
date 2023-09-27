6148564179f61e1a63186801ceee6c605f641e3e
phu phan <>
2023-09-27 16:30:43 +0700

    add universal link

--- src/component/AppTextUrl.tsx
+++ src/component/AppTextUrl.tsx
@@ -26,7 +26,17 @@ export const AppTextUrl = ({
   ...props
 }: Props) => {
   const handleUrlPress = async (url: string) => {
-    if (url.indexOf(DEEP_LINK) >= 0 || url.indexOf(OLD_DEEP_LINK) >= 0) {
+    let afterLink = url;
+    if (url.startsWith(DEEP_LINK)) {
+      afterLink = url.replace(new RegExp(`^${DEEP_LINK}/`), '');
+    } else if (url.startsWith(OLD_DEEP_LINK)) {
+      afterLink = url.replace(new RegExp(`^${OLD_DEEP_LINK}/`), '');
+    }
+    const arrayParamsLink = afterLink.split('/');
+
+    if (arrayParamsLink?.length > 2) {
+      handleDeepLink(url, true);
+    } else if (url.indexOf(DEEP_LINK) >= 0 || url.indexOf(OLD_DEEP_LINK) >= 0) {
       const link = await dynamicLinks().resolveLink(url);
       if (link?.url) {
         handleDeepLink(link?.url, true);
